fb05b79e04c2f62df0d54484ed7f40e69b278d43
Karla Calderón <karla@kycinsurance.com>
2018-08-13 11:04:01 -0700

    cookies advice

---
+++ admin/config.php
@@ -0,0 +1,16 @@
+<?php
+define('RUTA', 'https://www.beeinsured.co');
+
+
+$bd_config = array(
+    'basedatos' => 'blog_practica',
+    'usuario' => 'root'
+    'pass' => ''
+);
+
+$blog_config = array(
+    'post_por_pagina' => '2',
+    'carpeta_imagenes' => 'img/'
+)
+
+?>
